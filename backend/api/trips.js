d36e7d6d6b199d346be7aa6a0e02091e9269834c
Dillon Marshall <dillon>
2023-12-07 21:48:22 -0600

    working on funtionalities at home

--- backend/api/trips.js
+++ backend/api/trips.js
@@ -2,37 +2,75 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const admin = require('firebase-admin');
-const fs = require('fs');
 const parse = require('csv-parse');
-const upload = multer({ dest: 'uploads/' });
 const fs = require('fs');
+
+const upload = multer({ dest: 'uploads/' });
 
 // POST endpoint to import trips
 router.post('/import', upload.single('file'), (req, res) => {
-    const file = req.file;
-    // Read and parse the CSV file
-    const parser = fs.createReadStream(file.path)
-        .pipe(parse({
-            columns: true,
-            delimiter: ',', // Update delimiter if different
-        }));
-    const trips = [];
-    parser.on('data', (row) => {
-        trips.push(row);
+  const file = req.file;
+
+  // Read and parse the CSV file
+  const parser = fs
+    .createReadStream(file.path)
+    .pipe(
+      parse({
+        columns: true,
+        delimiter: ',', // Update delimiter if different
+      })
+    );
+
+  const trips = [];
+
+  parser.on('data', (row) => {
+    trips.push(row);
+  });
+
+  parser.on('end', () => {
+    // Insert each trip into Firebase
+    const tripsRef = admin.database().ref('trips');
+
+    trips.forEach((trip) => {
+      const newTripRef = tripsRef.push();
+      newTripRef.set(trip);
     });
-    parser.on('end', () => {
-        // Insert each trip into Firebase
-        const tripsRef = admin.database().ref('trips');
-        trips.forEach(trip => {
-            const newTripRef = tripsRef.push();
-            newTripRef.set(trip);
-        });
-        // Delete the file after processing
-        fs.unlinkSync(file.path);
-        res.status(200).send('Trips imported successfully');
-    });
-    parser.on('error', (error) => {
-        console.error(error);
-        res.status(500).send('Failed to import trips');
-    });
+
+    // Delete the file after processing
+    fs.unlinkSync(file.path);
+
+    res.status(200).send('Trips imported successfully');
+  });
+
+  parser.on('error', (error) => {
+    console.error(error);
+    res.status(500).send('Failed to import trips');
+  });
 });
+
+router.get('/trips', async (req, res) => {
+    try {
+        const filter = req.query;
+        const tripsRef = admin.database().ref('trips');
+        let query = tripsRef;
+
+        // Apply filters
+        if (filter.fromDate) {
+            query = query.orderByChild('date').startAt(filter.fromDate);
+        }
+        if (filter.toDate) {
+            query = query.endAt(filter.toDate);
+        }
+        // Add more filters as needed
+
+        // Fetch and return data
+        const snapshot = await query.once('value');
+        const trips = snapshot.val();
+        res.status(200).json(trips);
+    } catch (error) {
+        console.error('Error fetching filtered trips:', error);
+        res.status(500).send('Failed to fetch trips');
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,39 +2,107 @@ const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
 const app = express();
-
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const driversRouter = require('./api/drivers');
 const vehiclesRouter = require('./api/vehicles');
 const usersRouter = require('./api/users');
 const tripsRouter = require('./api/trips');
-
+const router = express.Router();
 // Firebase Admin initialization
 const serviceAccount = require('../firebase-admin-sdk.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://doc-hop-default-rtdb.firebaseio.com'
 });
-
-const db = admin.database();
+const db = admin.firestore();
 
 app.use('/api/drivers', driversRouter);
 app.use('/api/vehicles', vehiclesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/trips', tripsRouter);
+
+const csvParser = require('csv-parser');
+const fs = require('fs');
+
+router.get('/', async (req, res) => {
+  try {
+    const { fromDate, toDate, tripNumber, driver } = req.query;
+    let query = db.collection('trips');
+
+    // Add non-date filters directly to the query
+    if (tripNumber) {
+      query = query.where('Trip Number', '==', tripNumber);
+    }
+    if (driver) {
+      query = query.where('Driver', '==', driver);
+    }
+
+    const snapshot = await query.get();
+    const trips = [];
+    snapshot.forEach(doc => {
+      const trip = { id: doc.id, ...doc.data() };
+      // Filter by date in server code
+      if ((!fromDate || new Date(trip['Trip Date']) >= new Date(fromDate)) &&
+          (!toDate || new Date(trip['Trip Date']) <= new Date(toDate))) {
+        trips.push(trip);
+      }
+    });
+     res.json(trips);
+  } catch (error) {
+    console.error('Error fetching filtered trips:', error);
+    res.status(500).json({ message: 'Error fetching filtered trips', error: error.message });
+  }
+});
 // Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON bodies
 
-// Define a simple route that uses Firebase
-app.get('/api', (req, res) => {
-  // Example Firebase usage
-  const ref = db.ref('some/path');
-  ref.once('value', (snapshot) => {
-    res.json(snapshot.val());
-  }, (error) => {
-    res.status(500).json({ error: error.message });
-  });
+app.post('/api/uploadCSV', upload.single('file'), (req, res) => {
+  const csvFilePath = req.file.path;
+  const results = [];
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csvParser())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      // After parsing CSV file
+      processTrips(results)
+        .then((processedInfo) => {
+          res.status(200).json({
+            message: 'CSV processed successfully',
+            processedInfo // Details like number of trips added, skipped, etc.
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.message });
+        });
+    });
 });
+
+async function processTrips(trips) {
+  for (let trip of trips) {
+    const tripExists = await checkIfTripExists(trip['Trip Number']);
+    if (!tripExists) {
+      await addTripToFirebase(trip);
+    } else {
+      console.log(`Trip ${trip['Trip Number']} already exists`);
+      // Optionally send this info to frontend
+    }
+  }
+}
+
+async function checkIfTripExists(tripNumber) {
+  const tripRef = db.collection('trips').doc(tripNumber);
+  const doc = await tripRef.get();
+  return doc.exists;
+}
+
+
+async function addTripToFirebase(trip) {
+  const tripsRef = db.collection('trips'); // Assuming 'trips' is your collection in Firebase
+  await tripsRef.doc(trip['Trip Number']).set(trip);
+}
 
 // Start the server
 const PORT = process.env.PORT || 5000;
